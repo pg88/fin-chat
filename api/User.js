19f07461a820557eb029becb1dd4ca75fe349c4f
pg88 <pablo.a.garcia.e@gmail.com>
2019-07-22 23:33:02 -0500

    SOCKETIO - RABBITMQ
    Chat - saves, update, retrieves messages, events
    RabbitMQ - handle message, broadcaste message via socket to browser
    API - auth sigin method

--- api/User.js
+++ api/User.js
@@ -18,6 +18,33 @@ router.route("/signup").post((req, res, next) =>  {
       });
   });
 });
+router.route("/signin").get((req, res, next) =>  {
+  console.log("SGININ__", req.body);
+  res.setHeader("Content-Type", "application/json");
+  connectdb.then(db  =>  {
+    User.find({ email: req.body.email }, (err, user) => {
+      if(!user) {
+        return res.statusCode = 500;
+      }
+      user[0].comparePassword(req.body.password, (err, isMatch) => {
+        console.log("AAA__", isMatch);
+        if (err) {
+          throw err;
+        }
+        if(!isMatch) {
+          res.statusCode = 400;
+          return res.json({
+              message: "Wrong password"
+          })
+        }
+        res.statusCode = 200;
+        return res.json({
+          auth: true
+        });
+      })
 
+    })
+  })
+})
 
 module.exports  =  router;
